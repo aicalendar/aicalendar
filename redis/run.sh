40af633a6015c73f48f361ee2140e0a9cd731b3c
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-18 20:29:09 +0100

    running docker containers

---
+++ redis/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+source .env
+
+# Run the redis service
+docker run -d \
+  --name redis \
+  -e REDIS_PASSWORD=${REDIS_PASSWORD} \
+  -p ${REDIS_PORT}:${REDIS_PORT} \
+  redis:7.4.2-alpine \
+  sh -c "redis-server --requirepass $$REDIS_PASSWORD"
