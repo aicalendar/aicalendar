5c06d68fd6e976516afd76dda4e74df6b7760da3
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-18 23:27:39 +0100

    redis volume

--- redis/run.sh
+++ redis/run.sh
@@ -3,11 +3,14 @@
 # Load environment variables from .env file
 source .env
 
+docker volume create redis_data
+
 # Run the redis service
 docker run -d \
   --name redis \
   --restart unless-stopped \
   -e REDIS_PASSWORD=${REDIS_PASSWORD} \
   -p ${REDIS_PORT}:${REDIS_PORT} \
+  -v redis_data:/data \
   redis:7.4.2-alpine \
   sh -c "redis-server --requirepass ${REDIS_PASSWORD}"
